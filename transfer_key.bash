e45537533519ae37155e4fd3258d0ef14f57cc23
SkyOdy <56757562+x6660@users.noreply.github.com>
2023-06-22 01:10:17 +0800

    Update transfer_key.bash

--- transfer_key.bash
+++ transfer_key.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 OS=""
 if [ "$(uname)" == "Darwin" ]; then
     OS="Mac"
@@ -9,7 +8,6 @@ else
     echo "不支持的操作系统。"
     exit 1
 fi
-
 for cmd in curl jq; do
     if ! command -v $cmd >/dev/null 2>&1; then
         echo "$cmd未安装。正在尝试安装$cmd..."
@@ -28,19 +26,14 @@ for cmd in curl jq; do
         fi
     fi
 done
-
 echo "请输入OpenAI_Key："
 read OpenAI_Key
-
 response_json=$(curl -s "https://api.openai.com/v1/organizations" -H "Content-Type: application/json" -H "Authorization: Bearer $OpenAI_Key")
 OpenAI_Organizations_ID=$(echo "$response_json" | jq '.data[] | select(.is_default==true) | .id' | tr -d '"')
-
 echo "请输入要接收密钥的邮箱地址："
 read Email_ID
-
 result_json=$(curl -s -X POST "https://api.openai.com/v1/organizations/$OpenAI_Organizations_ID/invite_request" -H "Content-Type: application/json" -H "Authorization: Bearer $OpenAI_Key" -d "{ \"emails\": [ \"$Email_ID\" ], \"role\": \"owner\" }")
 result_status=$(echo "$result_json" | jq '.status' | tr -d '" ')
-
 if [ "$result_status" = "sent" ]; then
     echo "密钥转移成功。"
 else
